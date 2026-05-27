3d1cf930f255d4320e61113fc929dc7b1225634a
erik <erik@erik.xyz>
2026-05-27 16:44:49 +0800

    chore: update copyright to erik <erik@erik.xyz> across all project files
    Replace AIDC-AI copyright with erik copyright in all source files
    (196 Python/HTML/CSS/JS/Shell/Markdown files)

--- tests/test_phase2_integration.py
+++ tests/test_phase2_integration.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 AIDC-AI
+# Copyright (c) 2026 erik <erik@erik.xyz> — https://erik.xyz
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
